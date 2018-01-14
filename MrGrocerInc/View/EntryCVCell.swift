98d7a5947b363d3f59d10d8b0c10a639cb6eb103
MattOsak <Matt@Matts-iMac.local>
2018-01-14 12:55:59 -0500

    first commit

---
+++ MrGrocerInc/View/EntryCVCell.swift
@@ -0,0 +1,21 @@
+//
+//  EntryCVCell.swift
+//  MrGrocerInc
+//
+//  Created by Matt Osak on 2018-01-14.
+//  Copyright © 2018 Matt Osak. All rights reserved.
+//
+
+import UIKit
+
+class EntryCVCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageName: UIImageView!
+    
+    @IBOutlet weak var entryName: UILabel!
+    
+    @IBOutlet weak var EntryPrice: UILabel!
+    
+    
+    
+}
